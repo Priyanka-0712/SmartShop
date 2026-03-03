1d3831f9297f7df1e685cc0b04e497051e3bc119
Priyanka-0712 <priyankaboke801@gmail.com>
2026-03-03 18:52:09 +0530

    addmin operation code

--- src/com/products/Products.java
+++ src/com/products/Products.java
@@ -93,6 +93,223 @@ public class Products {
 	CallableStatement callableStatement=null;
 	Scanner scanner=new Scanner(System.in);
 	
+//=========================== Add Products =============================================
+			
+			public void addProduct() throws SQLException
+			{
+				List<Products> productsList=new ArrayList<Products>();
+				while(true)
+				{
+					System.out.println("Enter Product Name >> ");
+					product_name=scanner.nextLine().trim();
+					System.out.println("Enter Product Description >> ");
+					description=scanner.nextLine().trim();
+					System.out.println("Enter Product Price >> ");
+					price=Double.parseDouble(scanner.nextLine().trim());
+					System.out.println("Enter Product Quantity >> ");
+					quantity=Integer.parseInt(scanner.nextLine().trim());
+					productsList.add(new Products(product_name, description, price, quantity));
+					System.out.println("Do you Want To add Another Product yes//no ");
+					String choice=scanner.nextLine();
+					if(choice.equalsIgnoreCase("no"))
+					{
+						break;
+					}
+				}
+				try {
+					connection=TestConnection.getConnection();
+					connection.prepareStatement("insert into products\r\n"
+							+ "(product_name,description,price,quantity)values(?,?,?,?)");
+					for(Products products:productsList)
+					{
+						preparedStatement.setString(1, products.getProduct_name());
+						preparedStatement.setString(2, products.getDescription());
+						preparedStatement.setDouble(3, products.getPrice());
+						preparedStatement.setInt(4, products.getQuantity());
+						preparedStatement.addBatch();
+					}
+					preparedStatement.executeBatch();
+					System.out.println("Saving product to the database... ");
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}finally {
+					connection.close();
+					preparedStatement.close();
+				}
+				
+			}
+			
+//=================== View Product Stock ================================================
+
+			public void getProductStock() throws SQLException
+			{
+				try {
+					System.out.println("Enter Product ID to check stock >> ");
+					product_id=Integer.parseInt(scanner.nextLine().trim());
+					connection=TestConnection.getConnection();
+					preparedStatement=connection.prepareStatement
+							("select quantity from products where product_id=?");
+					preparedStatement.setInt(1, product_id);
+					resultSet=preparedStatement.executeQuery();
+					while(resultSet.next())
+					{
+						quantity=resultSet.getInt("quantity");
+					}
+					System.out.println("Available Quantity >> "+quantity);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				finally {
+					connection.close();
+					preparedStatement.close();
+					resultSet.close();
+				}
+				
+			}
+//========================== Update Product Details ===================================================================	 
+			
+			public void updateProductDetails() throws SQLException
+			{
+				try {
+					connection=TestConnection.getConnection();
+					System.out.println("Enter the Product ID to update >> ");
+					product_id=Integer.parseInt(scanner.nextLine().trim());
+					preparedStatement=connection.prepareStatement
+							("select * from products where product_id=?");
+					preparedStatement.setInt(1, product_id);
+					preparedStatement.executeQuery();
+					while(resultSet.next())
+					{
+						product_name=resultSet.getString("product_name");
+						description=resultSet.getString("description");
+						price=resultSet.getDouble("price");
+						quantity=resultSet.getInt("quantity");
+					}
+					System.out.println("Select field to update: ");
+					System.out.println("1. Product Name ");
+					System.out.println("2. Description ");
+					System.out.println("3. Price ");
+					System.out.println("4. Quantity ");
+					System.out.println();
+					System.out.println("Enter your choice >> ");
+					int choice=Integer.parseInt(scanner.nextLine().trim());
+					switch (choice) {
+					case 1:System.out.println("Enter new value >> ");
+							product_name=scanner.nextLine().trim();
+							preparedStatement=connection.prepareStatement
+									("update products set product_name=? where product_id=?");
+							preparedStatement.setString(1, product_name);
+							preparedStatement.setInt(2, product_id);
+						break;
+
+					case 2:System.out.println("Enter new value >> ");
+							description=scanner.nextLine().trim();
+							preparedStatement=connection.prepareStatement
+									("update products set description=? where product_id=?");
+							preparedStatement.setString(1, description);
+							preparedStatement.setInt(2, product_id);
+						break;
+						
+					case 3:System.out.println("Enter new value >> ");
+							price=Double.parseDouble(scanner.nextLine().trim());
+							preparedStatement=connection.prepareStatement
+									("update products set price=? where product_id=?");
+							preparedStatement.setDouble(1, price);
+							preparedStatement.setInt(2, product_id);
+						break;
+						
+					case 4:System.out.println("Enter new value >> ");
+							quantity=Integer.parseInt(scanner.nextLine().trim());
+							preparedStatement=connection.prepareStatement
+									("update products set quantity=? where product_id=?");
+							preparedStatement.setInt(1, quantity);
+							preparedStatement.setInt(2, product_id);
+						break;
+					default:
+						break;
+					}
+					preparedStatement.executeUpdate();
+					System.out.println("Updating product in database... ");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}finally {
+					connection.close();
+					preparedStatement.close();
+					resultSet.close();
+				}
+				
+			}
+			
+//====================== Delete Product From Inventory =========================================================
+			
+			public void deleteProduct() throws SQLException
+			{
+				try {
+					System.out.println("Enter the Product ID to delete >> ");
+					product_id=Integer.parseInt(scanner.nextLine().trim());
+					connection=TestConnection.getConnection();
+					preparedStatement=connection.prepareStatement
+							("delete from products where product_id=?");
+					preparedStatement.setInt(1, product_id);
+					System.out.println("Are you sure you want to delete this product? (Yes/No) >> ");
+					String choice=scanner.nextLine().trim();
+					{
+						if(choice.equalsIgnoreCase("no"))
+						{
+							return;
+						}
+					}
+					preparedStatement.executeUpdate();
+					System.out.println("Deleting product from database... ");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}finally {
+					connection.close();
+					preparedStatement.close();
+				}
+			}
+			
+//=================== View Registered Users ==========================================
+			
+				public void getRegisteredUsers() throws SQLException
+				{
+					
+					try {
+						connection=TestConnection.getConnection();
+						callableStatement=connection.prepareCall("{CALL view_registered_users()}");
+						resultSet=callableStatement.executeQuery();
+						ResultSetMetaData resultSetMetaData=resultSet.getMetaData();
+						int columnCount=resultSetMetaData.getColumnCount();
+						System.out.println("=====================================================================================");
+						for(int i=1;i<=columnCount;i++)
+						{
+							System.out.print(resultSetMetaData.getColumnLabel(i)+"\t|");
+						}
+						System.out.println();
+						System.out.println("=====================================================================================");
+						while(resultSet.next())
+						{
+							System.out.println(resultSet.getInt(1)+"\t|"+resultSet.getInt(2)+"\t|\r\n"
+						+resultSet.getInt(3)+"\t|"+resultSet.getInt(4)+"\t|"+resultSet.getInt(5)+"\t|");
+						}
+						
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					finally {
+						connection.close();
+						callableStatement.close();
+						resultSet.close();
+					}
+				}
+	
+	
 
 //=========================== Display All Products ====================================
 		
--- src/com/purchases/Purchases.java
+++ src/com/purchases/Purchases.java
@@ -89,7 +89,52 @@ public class Purchases {
 	ResultSet resultSet=null;
 	Scanner scanner=new Scanner(System.in);
 	
+//============== view user purchase history =============================================
+	
+		public void userPurchaseHistory() throws SQLException
+		{
+			try {
+				Users users=new Users();
+				connection=TestConnection.getConnection();
+				preparedStatement=connection.prepareStatement("select p.product_id as 'Product Id' ,\r\n"
+						+ "p.product_name as 'Product Name', pu.quantity as Quantity ,\r\n"
+						+ "pu.purchase_date as Date, pu.price_at_purchase as 'Price'\r\n" + 
+						" from users u join purchases pu on u.user_id=pu.user_id join\r\n"
+						+ "products p on pu.product_id=p.product_id where u.username=?;");
+				
+				System.out.println("Enter the Username to view their purchase history >> ");
+				String username=scanner.nextLine().trim();
+				username=users.getUsername();
+				preparedStatement.setString(1, username);
+				resultSet=preparedStatement.executeQuery();
+				ResultSetMetaData resultSetMetaData=resultSet.getMetaData();
+				int columnCount=resultSetMetaData.getColumnCount();
+				System.out.println("=====================================================================================");
+				for(int i=1;i<=columnCount;i++)
+				{
+					System.out.print(resultSetMetaData.getColumnLabel(i)+"\t|");
+				}
+				System.out.println();
+				System.out.println("=====================================================================================");
+				while(resultSet.next())
+				{
+					System.out.println(resultSet.getInt("Product Id")+"\t|"+resultSet.getString("Product Name")+"\t|\r\n"
+				+resultSet.getInt("Quantity")+"\t|"+resultSet.getDate("Date")+"\t|"+resultSet.getDouble("Price")+"\t|");
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally {
+				connection.close();
+				preparedStatement.close();
+				resultSet.close();
+			}
+		}
+		
+//============================================ End ==========================================================================
+	
+}
 
 	
-//============================================ End ==========================================================================
-}
+
