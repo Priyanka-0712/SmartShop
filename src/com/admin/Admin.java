19ace646ca4fef69010b55dc03fbb0db16a4e2cc
Amar Dhone <amardhone2015@gmail.com>
2026-03-04 11:17:43 +0530

    Added Project Again Resolved Conflicts

---
+++ src/com/admin/Admin.java
@@ -0,0 +1,62 @@
+package com.admin;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import com.products.Products;
+import com.purchases.Purchases;
+
+public class Admin {
+
+//============================= Admin Operation =============================================================
+	
+		Scanner scanner=new Scanner(System.in);
+		int input;
+		
+		public void adminOperation() throws SQLException
+		{
+			
+				Products products=new Products();
+				Purchases purchases=new Purchases();
+			do {
+				
+				System.out.println("===========================================");
+				System.out.println(" Please choose an option: ");
+				System.out.println(" 1. Add New Product  ");
+				System.out.println(" 2. View Product Stock");
+				System.out.println(" 3. View Registered Users ");
+				System.out.println(" 4. View User Purchase History ");
+				System.out.println(" 5. Update Product Details  ");
+				System.out.println(" 6. Delete Product from Inventory ");
+				System.out.println(" 7. Log Out");
+				System.out.println("===========================================");
+				System.out.println("Enter your choice >> ");
+				int choice=Integer.parseInt(scanner.nextLine().trim());
+				
+				switch (choice) {
+				case 1:products.addProduct();
+					break;
+
+				case 2:products.getProductStock();
+					break;
+					
+				case 3:products.getRegisteredUsers();
+					break;
+					
+				case 4:purchases.userPurchaseHistory();
+					break;
+					
+				case 5:products.updateProductDetails();
+					break;
+					
+				case 6:products.deleteProduct();
+					break;
+				
+				default:
+					break;
+				}
+			}while(input!=7);
+		}
+
+//============================================ End ==========================================================================
+}
---
+++ src/com/users/Users.java
@@ -0,0 +1,496 @@
+package com.users;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.admin.Admin;
+import com.connection.TestConnection;
+import com.customexception.MyException;
+import com.products.Products;
+
+public class Users {
+	
+//====================== Declaring Variables ===========================================
+	
+	private int user_id;
+	private String first_name;
+	private String last_name;
+	private String username;
+	private String password;
+	private String city;
+	private String email;
+	private String mobile;
+	private String role;
+
+//========================= Constructors ===============================================
+
+	public Users()
+	{
+		
+	}
+	
+	public Users(int user_id, String first_name, String last_name, String username, String password, String city,
+			String email, String mobile, String role) {
+		super();
+		this.user_id = user_id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.username = username;
+		this.password = password;
+		this.city = city;
+		this.email = email;
+		this.mobile = mobile;
+		this.role = role;
+	}
+	
+//===================== Getters Setters ==================================
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+//================== Connection Object Creation =======================================
+
+		Connection connection=null;
+		PreparedStatement preparedStatement=null;
+		CallableStatement callableStatement=null;
+		ResultSet resultSet=null;
+		Scanner scanner=new Scanner(System.in);
+		
+//======================= User Registration ======================================================
+
+//-------------------	User Registration form -----------------------------------------
+		
+		public void userRegistration() throws SQLException
+		{
+
+				System.out.println("Enter First Name >> ");
+				first_name=scanner.nextLine();
+				System.out.println("Enter Last Name >> ");
+				last_name=scanner.nextLine();
+				System.out.println("Enter Username >> ");
+				username=scanner.nextLine();
+				System.out.println("Enter Password >> ");
+				password=scanner.nextLine();
+				System.out.println("Enter City >> ");
+				city=scanner.nextLine();
+				System.out.println("Enter Email ID >> ");
+				email=scanner.nextLine();
+				System.out.println("Enter Mobile Number >> ");
+				mobile=scanner.nextLine();
+				System.out.println("Enter Your Role you are user or admin");
+				role=scanner.nextLine();
+				
+				try
+				{
+					connection=TestConnection.getConnection();
+					preparedStatement=connection.prepareStatement("select * from users");
+					resultSet=preparedStatement.executeQuery();
+					while(resultSet.next())
+					{
+					if(username.equals(resultSet.getString(4)))
+					try{
+						throw new MyException("Username Already Exists");
+					   }
+					catch (MyException e) {
+						System.out.println("====================================");
+						System.out.println("         "+e.getMessage());
+					}
+					}
+					
+					preparedStatement=connection.prepareStatement("insert into users(first_name,\r\n"
+						+ "last_name,username,password,city,email,mobile,role)values(?,?,?,?,?,?,?,?)");
+					preparedStatement.setString(1, first_name.trim());
+					preparedStatement.setString(2, last_name.trim());
+					preparedStatement.setString(3, username.trim());
+					preparedStatement.setString(4, password.trim());
+					preparedStatement.setString(5, city.trim());
+					preparedStatement.setString(6, email.trim());
+					preparedStatement.setString(7, mobile.trim());
+					preparedStatement.setString(8, role.toLowerCase().trim());
+				
+					try
+					{
+						try 
+						{
+							if(!validateEmail(email))
+							throw new MyException("Please Enter Valid Email id");
+							} catch (MyException e) {
+							System.out.println("====================================");
+							System.out.println("        "+e.getMessage());
+						}
+						
+						try
+						{
+							if(!validateMobile(mobile))
+							throw new MyException("Please Enter Valid 10 digit Mobile Number");
+						}
+						catch (MyException e) {
+							System.out.println("======================================");
+							System.out.println("         "+e.getMessage());
+						}
+						
+						try
+						{
+							if(!validateRole(role))
+								throw new MyException("Please Enter User or Admin Only");
+						}
+						catch (MyException e) {
+							System.out.println("====================================");
+							System.out.println("        "+e.getMessage());
+						}
+						if(mobile.length()!=10)
+						{
+							return;
+						}
+						
+						preparedStatement.executeUpdate();
+						System.out.println("========================================");
+						System.out.println("       "+"Registration Done !!!");
+						
+					}catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+				}
+				catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+					finally {
+								connection.close();
+								preparedStatement.close();
+								resultSet.close();	
+							}
+			}
+		
+//----------- checking valid 10 digit mobile number -----------------------------------
+		
+		public boolean validateMobile(String mobile)
+		{
+			for(int i=0;i<mobile.length();i++)
+			{
+				if(!Character.isDigit(mobile.charAt(i)))
+				{
+					return false;
+				}
+					
+			}
+			if((mobile.startsWith("6")||mobile.startsWith("7")||
+				mobile.startsWith("8")||mobile.startsWith("9"))&&(mobile.length()==10))
+				return true;
+			else 
+				return false;
+			
+		}
+		
+//------------  checking valid email id -----------------------------------------------
+		
+		public boolean validateEmail(String email)
+		{
+			if(email.contains("@")&&(email.endsWith(".com")))
+				return true;
+			else 
+				return false;				
+		}
+		
+//----------- checking role is user or admin only -------------------------------------
+		
+		public boolean validateRole(String role)
+		{
+			if(role.equals("admin")||role.equals("user"))
+				return true;
+			else
+				return false;
+		}
+
+//========================== User Login ===============================================
+	
+	public void usersLogin() throws SQLException
+	{
+		connection=TestConnection.getConnection();
+		preparedStatement=connection.prepareStatement("select * from users"
+				+ " where username=? and password=?");
+		int i=3;
+		while(i!=0)
+		{
+		System.out.println("Enter Your Username ");
+		username=scanner.nextLine();
+		System.out.println("Enter Your Password");
+		password=scanner.nextLine();
+		preparedStatement.setString(1, username);
+		preparedStatement.setString(2, password);
+		resultSet=preparedStatement.executeQuery();
+		{
+			if(resultSet.next())
+		{
+				
+			if(username.equals(resultSet.getString("username"))&&
+					password.equals(resultSet.getString("password")));
+			{
+				System.out.println("====================================================");
+				System.out.println("Login SuccessFull Welcome "+
+			resultSet.getString(2)+" "+resultSet.getString(3)+" !");
+				
+				user_id=resultSet.getInt(1);
+				role=resultSet.getString(9);
+				if(role.equals("admin"))
+				{
+					Admin admin=new Admin();
+					admin.adminOperation();
+				}
+				else if(role.equals("user"))
+				{
+					userOperation();
+				}
+			}
+			break;
+		}
+			else
+		{
+			System.out.println("=============================================");
+			System.out.println("Please Enter Correct Username or Password ");
+			if(i==0) {
+				System.out.println("Login Failed  No Attempts Left ");
+				return;
+			}
+			else if(i==1)
+			{
+				System.out.println("Last Attempt Remaining");
+			}else
+			{
+			System.out.println("Attempt Left "+i);
+			}
+			i--;
+		}
+			
+		}	
+		
+	  }
+		return;
+	}
+
+//======================   User Operation    ===========================================
+	
+	public void userOperation() throws SQLException
+	{
+				
+		Products products=new Products();
+		products.viewProductsList();
+		connection=TestConnection.getConnection();
+		preparedStatement=connection.prepareStatement("select product_id,\r\n"
+				+ "product_name,price,quantity from products");
+		resultSet=preparedStatement.executeQuery();
+		List<Products> list=new ArrayList<Products>();
+		
+		while(resultSet.next())
+		{
+			list.add(new Products(resultSet.getInt("product_id"), resultSet.getString("product_name"), resultSet.getDouble("price"), resultSet.getInt("quantity")));
+		}
+		
+		preparedStatement.close();
+		resultSet.close();
+		double price=list.get(0).getPrice();
+		System.out.println("Enter the Product ID to purchase >> ");
+		int id=Integer.parseInt(scanner.nextLine().trim());
+		System.out.println("Enter the Quantity >> ");
+		int quant=Integer.parseInt(scanner.nextLine().trim());
+		System.out.println("Checking stock availability...");
+		if(quant<products.getQuantity())
+		{
+			System.out.println(" Product is not available ");
+			return;
+		}
+		System.out.println("Adding product to your cart and saving purchase in database... ");
+		preparedStatement=connection.prepareStatement("insert into purchases(user_id,product_id,quantity,price_at_purchase)values(?,?,?,?)");
+		preparedStatement.setInt(1, user_id);
+		preparedStatement.setInt(2, id);
+		preparedStatement.setInt(3, quant);
+		preparedStatement.setDouble(4, price);
+		preparedStatement.executeUpdate();
+		preparedStatement.close();
+		System.out.println("Save purchase record immediately into the purchases. ");
+		
+		preparedStatement=connection.prepareStatement("update products set quantity=quantity-? where product_id=? and quantity>=?");
+		preparedStatement.setInt(1, quant);
+		preparedStatement.setInt(2, id);
+		preparedStatement.setInt(3, quant);
+		int rows=preparedStatement.executeUpdate();
+		if(rows>0)
+		{
+			System.out.println("Stock Updates Succesfully in Inventory");
+		}
+		else
+		{
+			System.out.println("Not Enough Stock Available");
+		}
+		
+		boolean found=false;
+		List<Products> cart=new ArrayList<Products>();
+		Products selectedProduct=null;
+		for(Products p:list)
+		{
+			if(p.getProduct_id()==id)
+			{
+				selectedProduct=p;
+				break;
+			}
+		}
+		for(Products p:cart)
+		{
+			if(p.getProduct_id()==id)
+			{
+				p.setQuantity(p.getQuantity()+quant);
+				found=true;
+				break;
+			}
+		}
+		if(!found)
+		{
+			Products cartProducts=new Products(selectedProduct.getProduct_id(),selectedProduct.getProduct_name(),selectedProduct.getPrice(),quant);
+			cart.add(cartProducts);
+		}
+		double totalAmount=0;
+		System.out.printf("%-20s | %-10s | %-10s | %-10s\n","Product Name" , "Quantity" , "Price" , "Subtotal");
+		System.out.println("===================================================================================");
+		for(Products p:cart)
+		{
+			double subtotal=p.getPrice()*p.getQuantity();
+			totalAmount+=subtotal;
+			System.out.printf("%-20s | %-10d | %-10.2f | %-10.2f\n",p.getProduct_name(),p.getQuantity(),p.getPrice(),subtotal);
+		}
+		System.out.println("===================================================================================");
+		System.out.printf("  Total Amount: %10.2f\n"+totalAmount);
+		
+		connection.close();
+		preparedStatement.close();
+		
+		
+	}
+	
+//======================== User Login First ===========================================================
+
+	public static void loginFirst()
+		{
+			try
+			{
+				throw new MyException("Please Login First");
+			}
+			catch (MyException e) {
+				System.out.println("================================");
+				System.out.println("     "+e.getMessage());
+			}
+		}
+
+//=======================  User Purchase History ==========================================================
+
+		public void getUserPurchaseHistory() throws SQLException
+		{
+			usersLogin();
+			System.out.println("Your Order History Is ");
+			connection=TestConnection.getConnection();
+			preparedStatement=connection.prepareStatement("SELECT\r\n" + 
+					"p.purchase_date as Date,\r\n" + 
+					"pr.product_name 'Product Name',\r\n" + 
+					"p.quantity As Quantity,\r\n" + 
+					"p.price_at_purchase AS 'Purchase Price',\r\n" + 
+					"(p.quantity*p.price_at_purchase) as Total\r\n" + 
+					"from purchases p join products pr \r\n" + 
+					"on p.product_id=pr.product_id \r\n" + 
+					"where p.user_id=?");
+			
+			preparedStatement.setInt(1, user_id);
+			resultSet=preparedStatement.executeQuery();
+			ResultSetMetaData resultSetMetaData=resultSet.getMetaData();
+			int columnCount=resultSetMetaData.getColumnCount();
+			System.out.println("=====================================================================================");
+			for(int i=1;i<=columnCount;i++)
+			{
+				System.out.print(resultSetMetaData.getColumnLabel(i)+"\t|");
+			}
+			System.out.println();
+			System.out.println("=====================================================================================");
+			while(resultSet.next())
+			{
+				System.out.print(resultSet.getDate("Date")+"\t|"+resultSet.getString("Product Name")
+				+"\t|"+resultSet.getString("Quantity")+"\t|"+resultSet.getString("Purchase Price")
+				+"\t|"+resultSet.getDouble("Total"));
+			}
+
+		}
+		
+//========================================= End =========================================================================
+}
